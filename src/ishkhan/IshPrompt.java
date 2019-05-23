d3b96aef31e63047b158b7fb3ec3c1ac6f0cead6
gilbertcarlson <gc@duoforajob.org>
2019-05-23 17:16:57 +0200

    test toFront behaviour on 1st floor

---
+++ src/ishkhan/IshPrompt.java
@@ -0,0 +1,39 @@
+package ishkhan;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class IshPrompt {
+    private JButton agreeButton;
+    private JPanel mainPanel;
+    private JLabel imageLabel;
+
+    public IshPrompt() {
+        agreeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("IshPrompt");
+        frame.setContentPane(new IshPrompt().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setExtendedState(JFrame.ICONIFIED);
+        boolean fullscreen;
+        fullscreen = false;
+        frame.setExtendedState(fullscreen ? JFrame.MAXIMIZED_BOTH : JFrame.NORMAL);
+        frame.setVisible(true);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        imageLabel = new JLabel(new ImageIcon("/Users/gilbertcarlson/IdeaProjects/IshPrompt/src/resources/images/DuoforaJob_logo_lowres-01.jpg"));
+    }
+}
